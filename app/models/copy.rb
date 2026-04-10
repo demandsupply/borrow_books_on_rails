fb4d414bf24ecb203b2bc5adc73631c3239f570c
demandsupply <giova.zol92@gmail.com>
2026-04-10 18:30:49 +0200

    feat: add active record callbacks

--- app/models/copy.rb
+++ app/models/copy.rb
@@ -9,4 +9,18 @@ class Copy < ApplicationRecord
 
   # VALIDATIONS
   validates :barcode, presence: true, uniqueness: true
+
+  
+  # ACTIVE RECORD CALLBACKS
+  # create a callback BEFORE checking validations (before_validation --> validation --> before_save --> save on db --> after_save)
+  before_validation :generate_barcode, on: :create
+
+
+  private
+
+  def generate_barcode
+    if self.barcode.nil? || self.barcode == false
+      self.barcode = "LIB-#{SecureRandom.hex(4).upcase}"
+    end
+  end
 end
--- app/models/loan.rb
+++ app/models/loan.rb
@@ -6,4 +6,32 @@ class Loan < ApplicationRecord
   enum status: { active: 0, returned: 1, overdue: 2 }
 
   # VALIDATIONS
+  # a user can have up to three active loans
+  validate :limit_user_loans, on: :create
+
+
+  # ACTIVE RECORD CALLBACKS
+  # calback: after a loan is created, mark that copy as "borrowed"
+  after_create :mark_copy_as_borrowed
+
+  # callback: after a copy state is updated to "returned", change that copy status to "available"
+  after_update :release_copy, if: :saved_change_to_returned?
+
+
+  # PRIVATE METHODS
+  private
+
+  def limit_user_loans
+    if user.active.loans.count > 3
+      errors.add(:user, "maximum amount of loans reached!")
+    end
+  end
+
+  def mark_copy_as_borrowed
+    copy.borrowed!
+  end
+
+  def release_copy
+    copy.available! if status == "returned"
+  end
 end
