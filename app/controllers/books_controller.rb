48827af46d86770d04f2d82f2e40430d71d155b7
demandsupply <giova.zol92@gmail.com>
2026-04-16 09:57:07 +0200

    feat: add book controller and views

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,30 @@
+class BooksController < ApplicationController
+  before_action :set_book, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @books = Book.all.order(title: :asc)
+  end
+
+  def show
+    @book_copies = @book.book_copies
+
+    # lately, consider to include :libraries in you want to show the library name for each book (eager loading, avoid N + 1 problem)  
+    # @book_copies = @book.book_copies.includes(:library)
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+
+  private
+
+  def set_book
+    @book = Book.find(params[:id])
+  end
+end
