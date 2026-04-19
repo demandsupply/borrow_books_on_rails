0cd6405b15bac762ed5f4347c171c948468a3bbe
demandsupply <giova.zol92@gmail.com>
2026-04-19 15:43:21 +0200

    feat: add field "description" to book model and execute db migration. Books#index now shows also the new added field

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_04_12_155319) do
+ActiveRecord::Schema[8.1].define(version: 2026_04_19_124710) do
   create_table "book_copies", force: :cascade do |t|
     t.string "barcode"
     t.integer "book_id", null: false
@@ -25,6 +25,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_04_12_155319) do
   create_table "books", force: :cascade do |t|
     t.string "author"
     t.datetime "created_at", null: false
+    t.text "description"
     t.string "isbn"
     t.string "title"
     t.datetime "updated_at", null: false
