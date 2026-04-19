677396df534a319d0d6d3c06149179ace2f34f0b
demandsupply <giova.zol92@gmail.com>
2026-04-19 14:36:14 +0200

    style: create and add minimal style to book_card partial. Add list of all books on books#index

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,5 @@
 class BooksController < ApplicationController
-  before_action :set_book, only: [:show, :edit, :update, :destroy]
+  before_action :set_book, only: [:show]
 
   def index
     @books = Book.all.order(title: :asc)
